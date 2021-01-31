b66e222ba93ee828e2371e3886149301c1db1570
Jo-Byr <63496507+Jo-Byr@users.noreply.github.com>
2021-01-31 09:09:23 +0100

    Bug correction
    The snake keeps its length throught its movements but can run through itself when asked the direction opposite to the current one.

--- snake.js
+++ snake.js
@@ -1,8 +1,8 @@
 const H = window.screen.height;
 const x = Math.floor((2/3)*H/25); //x represents the width of one of the elementary squares composing the snake, we want the canvas to be 25 squares wide
 const h = 25*x;
-var X = [(h+x)/2,(h-x)/2];    //Axis of the top left corner of the square
-var Y = [(h-x)/2,(h-x)/2];    //Ordinate of the top left corner of the square
+var X = [(h+x)/2,(h-x)/2,(h-3*x)/2];    //Axis of the top left corner of the square
+var Y = [(h-x)/2,(h-x)/2,(h-x)/2];    //Ordinate of the top left corner of the square
 cnv = document.getElementById("Canvas")
 
 ctx = cnv.getContext("2d");
@@ -18,77 +18,114 @@ function createCanvas(){ //Changing the canvas size to what we want
     ctx.closePath();
 
     ctx.fillStyle = 'white';    
-    ctx.beginPath();    //First elementary square
-    ctx.rect((h-x)/2,(h-x)/2,x,x);
-    ctx.rect((h+x)/2,(h-x)/2,x,x);
-    ctx.fill();
-    ctx.closePath();
+    //ctx.beginPath();    //First elementary square
+    ctx.fillRect((h-3*x)/2,(h-x)/2,x,x);
+    ctx.fillRect((h-x)/2,(h-x)/2,x,x);
+    ctx.fillRect((h+x)/2,(h-x)/2,x,x);
+    //ctx.fill();
+    //ctx.closePath();
 }
 
 var DIR = true;
+var PRESSED = false;
 
 function handleInputs(){
     document.addEventListener('keydown', (event) => {
-        if (event.key == "ArrowRight"){
+        if (event.key == "ArrowRight" && DIR != "left"){
             DIR = "right";
         }
-        else if (event.key == "ArrowLeft"){
+        else if (event.key == "ArrowLeft" && DIR != "right"){
             DIR = "left";
         }
-        else if (event.key == "ArrowUp"){
+        else if (event.key == "ArrowUp" && DIR != "down"){
             DIR = "up";
         }
-        else if (event.key == "ArrowDown"){
+        else if (event.key == "ArrowDown" && DIR != "up"){
             DIR = "down";
+        }
+        if (PRESSED == false){
+            move()
+            PRESSED = true
         }
     })
 }
 
 function move(){
-    console.log(X,Y)
     if (X[0]+x<h && DIR == "right"){
-        ctx.fillStyle = 'black';
-        ctx.fillRect(X[X.length-1], X[X.length-1], x, x);
+        ctx.fillStyle = 'black';    //Deleting ancient square
+        ctx.beginPath();
+        ctx.rect(X[X.length-1],Y[X.length-1],x,x);
+        ctx.fill();
+        ctx.closePath();
 
         ctx.fillStyle = 'white';    //Creating a square at the new position
-        ctx.fillRect(X[0]+x,Y[0],x,x);
+        ctx.beginPath();
+        ctx.rect(X[0]+x,Y[0],x,x);
+        ctx.fill();
+        ctx.closePath();
 
-        for (k=0;k<X.length;k++){
-            X[k] = X[k]+x;
+        for (k=X.length-1;k>0;k--){
+            X[k] = X[k-1];
+            Y[k] = Y[k-1];
         }
+        X[0] += x;
     }
     else if (X[0]>0 && DIR == "left"){
-        ctx.fillStyle = 'black';
-        ctx.fillRect(X[X.length-1], X[X.length-1], x, x);
+        ctx.fillStyle = 'black';    //Deleting ancient square
+        ctx.beginPath();
+        ctx.rect(X[X.length-1],Y[X.length-1],x,x);
+        ctx.fill();
+        ctx.closePath();
 
         ctx.fillStyle = 'white';    //Creating a square at the new position
-        ctx.fillRect(X[0]-x,Y[0],x,x);
+        ctx.beginPath();
+        ctx.rect(X[0]-x,Y[0],x,x);
+        ctx.fill();
+        ctx.closePath();
 
-        for (k=0;k<X.length;k++){
-            X[k] = X[k]-x;
+        for (k=X.length-1;k>0;k--){
+            X[k] = X[k-1];
+            Y[k] = Y[k-1];
         }
+        X[0] -= x;
     }
     else if (Y[0]>0 && DIR == "up"){
-        ctx.fillStyle = 'black';
-        ctx.fillRect(X[X.length-1], X[X.length-1], x, x);
+        ctx.fillStyle = 'black';    //Deleting ancient square
+        ctx.beginPath();
+        ctx.rect(X[X.length-1],Y[X.length-1],x,x);
+        ctx.fill();
+        ctx.closePath();
 
         ctx.fillStyle = 'white';    //Creating a square at the new position
-        ctx.fillRect(X[0],Y[0]-x,x,x);
+        ctx.beginPath();
+        ctx.rect(X[0],Y[0]-x,x,x);
+        ctx.fill();
+        ctx.closePath();
 
-        for (k=0;k<Y.length;k++){
-            Y[k] = Y[k]-x;
+        for (k=X.length-1;k>0;k--){
+            X[k] = X[k-1];
+            Y[k] = Y[k-1];
         }
+        Y[0] -= x;
     }
     else if (Y[0]+x<h && DIR == "down"){
-        ctx.fillStyle = 'black';
-        ctx.fillRect(X[X.length-1], X[X.length-1], x, x);
+        ctx.fillStyle = 'black';    //Deleting ancient square
+        ctx.beginPath();
+        ctx.rect(X[X.length-1],Y[X.length-1],x,x);
+        ctx.fill();
+        ctx.closePath();
 
         ctx.fillStyle = 'white';    //Creating a square at the new position
-        ctx.fillRect(X[0],Y[0]+x,x,x);
+        ctx.beginPath();
+        ctx.rect(X[0],Y[0]+x,x,x);
+        ctx.fill();
+        ctx.closePath();
 
-        for (k=0;k<Y.length;k++){
-            Y[k] = Y[k]+x;
+        for (k=X.length-1;k>0;k--){
+            X[k] = X[k-1];
+            Y[k] = Y[k-1];
         }
+        Y[0] += x;
     }
     setTimeout(move, 500);
 }
